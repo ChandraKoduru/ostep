e9dbea803ca76018b8de117b1454631d857195d1
Chandra Koduru <kscraja@gmail.com>
2015-03-20 11:20:53 +0530

    concurrent queue and hash_table

---
+++ concurrency/concurrent_hash_table.c
@@ -0,0 +1,36 @@
+//http://pages.cs.wisc.edu/~remzi/OSTEP/threads-locks-usage.pdf
+#include <stdio.h>
+#include <pthread.h>
+#define BUCKETS (101)
+
+typedef struct __node_t {
+    int key;
+    struct __node_t *next;
+} node_t;
+
+typedef struct __list_t {
+    node_t *head;
+    pthread_mutex_t lock;
+} list_t;
+
+typedef struct __hash_t {
+    list_t lists[BUCKETS];
+} hash_t;
+
+void Hash_Init(hash_t *H) {
+    int i;
+    for (i = 0; i < BUCKETS; i++) {
+        List_Init(&H->lists[i]);
+    }
+}
+
+int Hash_Insert(hash_t *H, int key) {
+    int bucket = key % BUCKETS;
+    return List_Insert(&H->lists[bucket], key);
+}
+
+int Hash_Lookup(hash_t *H, int key) {
+    int bucket = key % BUCKETS;
+    return List_Lookup(&H->lists[bucket], key);
+}
+
