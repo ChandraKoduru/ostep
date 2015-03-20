e1689872802e1dc5d9a1a070e9d54cb9b0842ef7
Chandra Koduru <kscraja@gmail.com>
2015-03-20 10:59:52 +0530

    concurrent linked list and counter

---
+++ concurrency/counter_without_locks.c
@@ -0,0 +1,20 @@
+//http://pages.cs.wisc.edu/~remzi/OSTEP/threads-locks-usage.pdf
+typedef struct __counter_t {
+    int value;
+} counter_t;
+
+void init(counter_t *c) {
+    c->value = 0;
+}
+
+void increment(counter_t* c) {
+    c->value++;
+}
+
+void decrement(counter_t* c) {
+    c->value--;
+}
+
+int get(counter_t* c) {
+    return c->value;
+}
