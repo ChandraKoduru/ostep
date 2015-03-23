146036f97c625d9d3378d9d119afd233a3153bea
Chandra Koduru <chandrakoduru@outlook.com>
2015-03-23 07:15:31 +0530

    locks with queue behavior

--- concurrency/locks/test_and_set.c
+++ concurrency/locks/test_and_set.c
@@ -1,20 +1,10 @@
 //http://pages.cs.wisc.edu/~remzi/OSTEP/threads-locks.pdf
-
 // c-implementation of test_set_atomic_exchange machine instruction
+#include <lock.h>
 int TestAndSet(int *old_ptr, int new) {
     int old = *old_ptr; //fetch old value at old_ptr
     *old_ptr = new; //store 'new' into old_ptr
     return old;
-}
-
-typedef struct __lock_t {
-    int flag;
-} lock_t;
-
-void init(lock_t *lock) {
-    //0 - indicates that lock is available
-    //1 - indicates lock not available
-    lock->flag = 0; //initially available
 }
 
 void lock(lock_t *lock) {
